eb262e156d6bc8772f0abc0e50785de1d4c6b98e
Nagendra219 <76140851+Nagendra219@users.noreply.github.com>
2025-07-21 17:25:48 +0530

    Main.java

--- vitals/Main.java
+++ vitals/Main.java
@@ -5,6 +5,18 @@ public class Main {
          return isTemperatureOk() && isSocOk() && isChargeRateOk();
     }
 
+    private boolean isTemperatureOk() {
+        return temperature >= 0 && temperature <= 45;
+    }
+
+    private boolean isSocOk() {
+        return soc >= 20 && soc <= 80;
+    }
+
+    private boolean isChargeRateOk() {
+        return chargeRate <= 0.8;
+    }
+
     public static void main(String[] args) {
         assert(batteryIsOk(25, 70, 0.7f) == true);
         assert(batteryIsOk(50, 85, 0.0f) == false);
